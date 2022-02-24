93eff6d6a4e13e3babef3ff4a7484385cbf0a77e
Arthur Wu <warthur123@gmail.com>
2022-02-24 09:22:54 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+
+const PORT = process.env.PORT || 8000;
+
+const app = express();
+
+// Middleware //
+app.use(cors());
+app.use(express.json());
+
+
+// Routes //
+// GET get all appointments
+app.get('/all', async (req, res) => {
+    try {
+        res.send("Hello World");
+    } catch (error) {
+        console.error(error);
+    }
+})
+
+
+app.use(express.static(path.join(__dirname, "/client/build")));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
+});
+
+
+app.listen(PORT, () =>
+    console.log(`app listening on port ${PORT}!`))
